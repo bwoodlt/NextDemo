13d0743231d7222376f9e7798015c469fb8aa3f6
Kunle <kunle_adetayo@just-fastfood.com>
2017-04-02 17:50:36 +0100

    added index.js

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+class NextApp extends React.Component {
+    render() {
+        return (
+            <section>
+                <h1>Welcome Home!</h1>
+                <p>How was it?</p>
+            </section>
+        )
+    }
+}
+
+export default NextApp;
